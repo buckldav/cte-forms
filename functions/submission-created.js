0a4a866a7ea407a526a7c23a34b9a198442526c1
David Buckley <runalldayandnight@gmail.com>
2022-12-20 19:52:23 -0700

    log better

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
   const body = JSON.parse(event.body);
-  console.log(`Received a submission: ${body}`);
+  console.log(`Received a submission: ${JSON.stringify(body)}`);
   return { statusCode: 200, body };
 };
