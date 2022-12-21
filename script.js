3393c8675c54ec72ac8c9f853f96a75987eecc79
David Buckley <runalldayandnight@gmail.com>
2022-12-20 19:33:29 -0700

    init

---
+++ script.js
@@ -0,0 +1,77 @@
+const form = document.querySelector("form");
+const storables = {
+  instructorsName: {
+    element: document.querySelector("#instructorsName"),
+  },
+  school: { element: document.querySelector("#school") },
+  district: { element: document.querySelector("#district") },
+};
+
+function studentRows() {
+  const addRow = document.querySelector("#addRow");
+  const removeRow = document.querySelector("#removeRow");
+  let row = 0;
+  const addRowFunc = (event) => {
+    event ? event.preventDefault() : null;
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td><input name="name${row}" id="name${row}" /></td>
+      <td><input name="reason${row}" id="reason${row}" /></td>
+    `;
+    document.querySelector("tbody").appendChild(tr);
+    row++;
+  };
+  const removeRowFunc = (event) => {
+    event ? event.preventDefault() : null;
+    document.querySelector("tbody>:last-child").remove();
+    row--;
+  };
+  const disabler = () => {
+    if (row <= 1) {
+      removeRow.disabled = true;
+    } else {
+      removeRow.disabled = false;
+    }
+  };
+  addRow.onclick = (event) => {
+    addRowFunc(event);
+    disabler();
+  };
+  addRowFunc();
+  removeRow.onclick = (event) => {
+    removeRowFunc(event);
+    disabler();
+  };
+}
+
+function initAndComputeValues() {
+  const testDate = document.querySelector("#testDate");
+  testDate.valueAsDate = new Date();
+  Object.keys(storables).forEach((key) => {
+    storables[key].element.value = localStorage.getItem(key);
+  });
+  form.onchange = (event) => {
+    console.log(event.target.id);
+    if (Object.keys(storables).includes(event.target.id)) {
+      storables[event.target.id].value = event.target.value;
+      localStorage.setItem(event.target.id, event.target.value);
+    }
+  };
+}
+
+function clearValues() {
+  const clearButton = document.querySelector("#clearButton");
+  clearButton.onclick = (event) => {
+    localStorage.clear();
+    form.reset();
+  };
+}
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  console.log(new FormData(form));
+};
+
+studentRows();
+initAndComputeValues();
+clearValues();
